a431c0a2d776b31009793af5ca991055c10aae05
Tianlin Zhang <tianlinzhang@gmail.com>
2014-12-04 10:30:11 +0000

    Slight version change
    Whoops
    Apparently I don't understand how revert works

--- dist/bpoverlay.min.user.js
+++ dist/bpoverlay.min.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BombParty Overlay
-// @version      1.0.3
+// @version      1.1.0
 // @description  Overlay + Utilities for BombParty!
 // @icon         https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon.png
 // @icon64       https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon64.png
